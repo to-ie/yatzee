47689f898d5f29c2ec746f7934645a72475c1f76
to-ie <theodorekall@protonmail.com>
2023-05-17 15:26:57 +0100

    finish design of score and setup of db

--- app/models.py
+++ app/models.py
@@ -9,15 +9,39 @@ class Game(db.Model):
     playerthree = db.Column(db.String(64), index=True, unique=True)
     playerfour = db.Column(db.String(64), index=True, unique=True)
     playerfive = db.Column(db.String(64), index=True, unique=True)
-    
 
     def __repr__(self):
         return ''.format(self.id)
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+
+    name = db.Column(db.String(64), index=True)
+
+    ones = db.Column(db.Integer(), index=True)
+    twos = db.Column(db.Integer(), index=True)
+    threes = db.Column(db.Integer(), index=True)
+    fours = db.Column(db.Integer(), index=True)
+    fives = db.Column(db.Integer(), index=True)
+    sixed = db.Column(db.Integer(), index=True)
+    subtotalupper = db.Column(db.Integer(), index=True)
+    bonus = db.Column(db.Integer(), index=True, default=0)
+    totaluppersection = db.Column(db.Integer(), index=True)
+
+    threex = db.Column(db.Integer(), index=True)
+    fourx = db.Column(db.Integer(), index=True)
+    fullhouse = db.Column(db.Integer(), index=True)
+    small = db.Column(db.Integer(), index=True)
+    large = db.Column(db.Integer(), index=True)
+    yahtzee = db.Column(db.Integer(), index=True)
+    chance = db.Column(db.Integer(), index=True)
+    totallower = db.Column(db.Integer(), index=True)
+    bonus = db.Column(db.Integer(), index=True, default=0)
     
+    total = db.Column(db.Integer(), index=True)
+
+
+
 
     def __repr__(self):
         return ''.format(self.id)
